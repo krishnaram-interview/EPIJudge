48b0e6a518691c5c01343f0687986ae85993f10d
Ramakrishna Shastri <ramakrishna.shastri@target.com>
2021-08-15 12:14:30 +0530

    Stacks MaxAPI: Solution

--- epi_judge_java/epi/StackWithMax.java
+++ epi_judge_java/epi/StackWithMax.java
@@ -4,28 +4,100 @@ import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
-public class StackWithMax {
 
-  public static class Stack {
-    public boolean empty() {
-      // TODO - you fill in here.
-      return true;
-    }
-    public Integer max() {
-      // TODO - you fill in here.
-      return 0;
-    }
-    public Integer pop() {
-      // TODO - you fill in here.
-      return 0;
-    }
-    public void push(Integer x) {
-      // TODO - you fill in here.
-      return;
+public class StackWithMax {
+  public static class StackElement {
+    public Integer element;
+    public Integer count;
+
+    public StackElement(Integer element, Integer count) {
+      this.element = element;
+      this.count = count;
     }
   }
+
+  // Best case approach is when the max value is tracked during push and pop, right from the beginning.
+  // With Time Complexity -> O(1) Space Complexity -> O(n)
+  public static class Stack {
+    private Deque<Integer> stack = new LinkedList<>();
+    private Deque<StackElement> auxStack = new LinkedList<>();
+
+    public boolean empty() {
+      // TODO - you fill in here.
+      return stack.isEmpty();
+    }
+
+    public Integer max() {
+      // TODO - you fill in here.
+      return auxStack.peek() != null ? auxStack.peek().element : null;
+    }
+
+    public Integer pop() {
+      // TODO - you fill in here.
+      Integer x = stack.pop();
+      if (!auxStack.isEmpty() && x.equals(auxStack.peek().element)) {
+        if (auxStack.peek().count > 1) {
+          auxStack.peek().count--;
+        } else {
+          auxStack.pop();
+        }
+      }
+      return x;
+    }
+
+    public void push(Integer x) {
+      // TODO - you fill in here.
+      if (auxStack.isEmpty()) {
+        auxStack.push(new StackElement(x, 1));
+      } else if (x > auxStack.peek().element) {
+        auxStack.push(new StackElement(x, 1));
+      } else if (x.equals(auxStack.peek().element)) {
+        auxStack.peek().count++;
+      }
+      stack.push(x);
+    }
+  }
+
+//  // Brute force approach: Takes O(n) time to find max value in worst case and O(1) -> Space complexity
+//  public static class Stack {
+//    private Deque<Integer> stack = new LinkedList<>();
+//
+//    public boolean empty() {
+//      // TODO - you fill in here.
+//      return stack.isEmpty();
+//    }
+//
+//    public Integer max() {
+//      // TODO - you fill in here.
+//      Iterator<Integer> stackItr = stack.iterator();
+//      Integer max = null;
+//      while (stackItr.hasNext()) {
+//        Integer val = stackItr.next();
+//        if (max == null) {
+//          max = val;
+//        } else if (val > max) {
+//          max = val;
+//        }
+//      }
+//      return max;
+//    }
+//
+//    public Integer pop() {
+//      // TODO - you fill in here.
+//      return stack.pop();
+//    }
+//
+//    public void push(Integer x) {
+//      // TODO - you fill in here.
+//     stack.push(x);
+//    }
+//  }
+
   @EpiUserType(ctorParams = {String.class, int.class})
   public static class StackOp {
     public String op;
