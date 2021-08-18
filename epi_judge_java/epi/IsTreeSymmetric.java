00d2caf70f5a45744992f22e2cdf23ffb5bb4329
Ramakrishna Shastri <ramakrishna.shastri@target.com>
2021-08-18 23:37:53 +0530

    Symmetric tree: Solution

--- epi_judge_java/epi/IsTreeSymmetric.java
+++ epi_judge_java/epi/IsTreeSymmetric.java
@@ -1,4 +1,8 @@
 package epi;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class IsTreeSymmetric {
@@ -6,7 +10,65 @@ public class IsTreeSymmetric {
 
   public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
     // TODO - you fill in here.
-    return true;
+    //return mySolution(tree);
+    return bestSolution(tree);
+  }
+
+  // Time complexity -> O(n) and space complexity -> O(h)
+  private static boolean bestSolution(BinaryTreeNode<Integer> root) {
+    if (root == null) {
+      return true;
+    } else {
+      return checkSymmetry(root.left, root.right);
+    }
+  }
+
+  private static boolean checkSymmetry(BinaryTreeNode<Integer> subTree1, BinaryTreeNode<Integer> subTree2) {
+    if (subTree1 == null && subTree2 == null) {
+      return true;
+    } else if (subTree1 != null && subTree2 != null) {
+      return subTree1.data.equals(subTree2.data) &&
+              checkSymmetry(subTree1.left, subTree2.right) &&
+              checkSymmetry(subTree1.right, subTree2.left);
+    } else {
+      return false;
+    }
+  }
+
+  // Time complexity -> O(n) and Space complexity -> O(n)
+  // This is slow coz, has to perform reverse and must traverse entire tree all the time.
+  private static boolean mySolution(BinaryTreeNode<Integer> tree) {
+    if (tree == null) {
+      return true;
+    } else {
+      List<Integer> leftTreeResult = inOrderData(tree.left);
+      List<Integer> rightTreeResult = inOrderData(tree.right);
+      Collections.reverse(rightTreeResult);
+      return leftTreeResult.equals(rightTreeResult);
+    }
+  }
+
+  private static List<Integer> inOrderData(BinaryTreeNode<Integer> root) {
+    if (root == null) {
+      return new ArrayList<>();
+    }
+
+    List<Integer> result = new ArrayList<>();
+    List<Integer> left = inOrderData(root.left);
+
+    if (!left.isEmpty()) {
+      result.addAll(left);
+    }
+
+    result.add(root.data);
+
+    List<Integer> right = inOrderData(root.right);
+
+    if (!right.toString().isBlank()) {
+      result.addAll(right);
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
