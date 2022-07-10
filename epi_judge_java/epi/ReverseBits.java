e4528100c0fab280c3d8ac784fe5523637126500
Ramakrishna Shastri <ramakrishna.shastri@target.com>
2022-07-10 11:23:03 +0530

    Bits: reverse

--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -5,7 +5,21 @@ public class ReverseBits {
   @EpiTest(testDataFile = "reverse_bits.tsv")
   public static long reverseBits(long x) {
     // TODO - you fill in here.
-    return 0;
+    return bruteForce(x);
+  }
+
+  private static long bruteForce(long x) {
+    for (int i=0; i<32; i++) {
+      x = swapBits(x, i, 63-i);
+    }
+    return x;
+  }
+
+  private static long swapBits(long x, int i, int j) {
+    if ((x >> i & 1) != (x >> j & 1)) {
+      x = x ^ (1L << i | 1L << j);
+    }
+    return x;
   }
 
   public static void main(String[] args) {
