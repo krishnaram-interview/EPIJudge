652c44ff10556ee0528c4621760066b7b7514569
z004387 <ramakrishna.shastri@target.com>
2022-07-24 23:16:36 +0530

    Stacks and Queues: Sols

--- epi_judge_java/epi/DirectoryPathNormalization.java
+++ epi_judge_java/epi/DirectoryPathNormalization.java
@@ -1,4 +1,8 @@
 package epi;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class DirectoryPathNormalization {
@@ -6,7 +10,52 @@ public class DirectoryPathNormalization {
 
   public static String shortestEquivalentPath(String path) {
     // TODO - you fill in here.
-    return "";
+
+    if (path == "") {
+      return path;
+    }
+    String[] pathStr = path.split("/");
+    Deque<String> normalizedPathStr = new LinkedList<>();
+
+    if (path.startsWith("/")) {
+      normalizedPathStr.push("/");
+    }
+
+    for(int i=0; i<pathStr.length; i++) {
+      switch (pathStr[i]) {
+        case ".":
+        case "":
+          break;
+        case "..":
+          if (normalizedPathStr.isEmpty() || normalizedPathStr.peek().equals("..")) {
+            normalizedPathStr.push("..");
+          } else {
+            normalizedPathStr.pop();
+          }
+          break;
+        default:
+          normalizedPathStr.push(pathStr[i]);
+          break;
+      }
+    }
+
+    StringBuilder result = new StringBuilder();
+
+    if (!normalizedPathStr.isEmpty()) {
+      Iterator<String> stringIterator = normalizedPathStr.descendingIterator();
+      String val = stringIterator.next();
+      result.append(val);
+
+      while (stringIterator.hasNext()) {
+        if (!val.equals("/")) {
+          result.append("/");
+        }
+        val = stringIterator.next();
+        result.append(val);
+      }
+    }
+
+    return result.toString();
   }
 
   public static void main(String[] args) {
--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -1,4 +1,7 @@
 package epi;
+import java.util.Deque;
+import java.util.LinkedList;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class IsValidParenthesization {
@@ -6,7 +9,75 @@ public class IsValidParenthesization {
 
   public static boolean isWellFormed(String s) {
     // TODO - you fill in here.
-    return true;
+    // O(n) time and O(1) space
+    // return bookSol(s);
+
+    // O(n) time and O(1) space
+    return mySol(s);
+  }
+
+
+  private static boolean bookSol(String s) {
+    Deque<Character> leftBrackets = new LinkedList<>();
+    for (int i = 0; i< s.length(); i++) {
+      if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+        leftBrackets.push(s.charAt(i));
+      } else {
+        if (leftBrackets.isEmpty()) {
+          return false;
+        } else {
+          if ((s.charAt(i) == ')' && leftBrackets.peek() == '(') ||
+                  (s.charAt(i) == '}' && leftBrackets.peek() == '{') ||
+                  (s.charAt(i) == ']' && leftBrackets.peek() == '[')
+          ) {
+            leftBrackets.pop();
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+    return leftBrackets.isEmpty();
+  }
+
+  private static boolean mySol(String s) {
+    Deque<Character> stack = new LinkedList<>();
+    boolean isWellFormed = true;
+    for (int i = 0; i< s.length() && isWellFormed; i++) {
+      char c1 = s.charAt(i);
+      switch (c1) {
+        case '{':
+        case '(':
+        case '[':
+          stack.push(c1);
+          break;
+        case '}':
+          isWellFormed = checkIfWellFormed(stack, '{');
+          break;
+        case ')':
+          isWellFormed = checkIfWellFormed(stack, '(');
+          break;
+        case ']':
+          isWellFormed = checkIfWellFormed(stack, '[');
+          break;
+        default:
+          break;
+      }
+    }
+    return isWellFormed && stack.isEmpty();
+  }
+
+  private static boolean checkIfWellFormed(Deque<Character> stack, Character c) {
+    boolean isWellFormed = true;
+    if (!stack.isEmpty()) {
+      char c2 = stack.pop();
+      if (c2 != c) {
+        isWellFormed = false;
+      }
+    } else {
+      isWellFormed = false;
+    }
+    return isWellFormed;
   }
 
   public static void main(String[] args) {
